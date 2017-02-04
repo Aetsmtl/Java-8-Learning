7cf382738f39a7f756ca9597504d5bb43a303331
aetsmtl <arnold.fotsing.nde@esigetel.net>
2017-02-04 14:53:20 +0100

    simple project with spring

--- src/main/java/staring/FoodApp.java
+++ src/main/java/staring/FoodApp.java
@@ -1,12 +1,21 @@
 package staring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 public class FoodApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Vegetable v = new Vegetable();
-		Animal a = new Animal();
+		//Vegetable v = new Vegetable();
+		//Animal a = new Animal();
+		
+		ApplicationContext appContext = new FileSystemXmlApplicationContext("AppContext.xml");
+		
+		Vegetable v = appContext.getBean("vegetable", Vegetable.class);
+		Animal a = (Animal) appContext.getBean("animal");
+		
 		
 		System.out.println(v.talkAboutMe());
 		System.out.println(a.talkAboutMe());

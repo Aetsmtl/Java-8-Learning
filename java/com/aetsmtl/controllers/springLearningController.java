7316c8fe68b72fbb3a05330e9db270b274e991bb
aetsmtl <arnold.fotsing.nde@esigetel.net>
2017-02-10 15:28:48 +0100

    creation of our view

--- java/com/aetsmtl/controllers/springLearningController.java
+++ java/com/aetsmtl/controllers/springLearningController.java
@@ -1,5 +1,7 @@
 package com.aetsmtl.controllers;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class springLearningController {
 
-	@RequestMapping(value = "/quote.html")
+	private String[] quote = {"To be or not be - Sheckpear",
+			 "Alors que la pluspart des gens rêvent au succes, les gagnants sont"
+			 + " bien reveillés et travaillent dur pour y arriver - Anonyme",
+			 "La vie appartient à ceux qui se lèvent tôt",
+			 "Le pire n'est pas de tomber mais c'est de ne pas pouvoir se relever"};
+	
+	@RequestMapping(value = "/getQuote")
 	public String getRandomQote(Model model){
-		model.addAttribute("Random quote");
-		return "Quote";
+		
+		int rankQuote = new Random().nextInt(quote.length);
+		String randomQuote = quote[rankQuote];
+		model.addAttribute("randomQuote", randomQuote);
+		return "quote" ;
 	}
 }
